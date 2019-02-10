735dd933623c5b9749dce3a2c15f34cea5c24071
Ronan <roleary81@gmail.com>
2019-02-10 17:15:43 +0100

    add whatever I added

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,7 +46,7 @@ class HomeIndex extends React.Component {
                         <article style={{backgroundImage: `url(${pic01})`}}>
                             <header className="major">
                                 <h3>Experience</h3>
-                                <p>Been around for a long long year...</p>
+                                <p>Been around for a long, long year...</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
