8ee4032a339208889a793e1501ceab4a52a2b33a
Ronan <roleary81@gmail.com>
2019-02-10 17:03:18 +0100

    hehehehehe

--- src/components/BannerLanding404.js
+++ src/components/BannerLanding404.js
@@ -3,8 +3,11 @@ import React from 'react'
 const BannerLanding404 = (props) => (
     <section id="banner" className="style2 bannerLanding">
         <div className="inner">
-            <header className="major">
-                <h1>4 OH 4!!!!</h1>
+            <header className="404">
+                <h1>4-OH-4!</h1>
+                <p>Hmmm...</p>
+                <p>Hmmm...either you tried going to a route that doesn't exist, <br />
+                or you broke something. I'd bail if I were you</p>
             </header>
         </div>
     </section>
