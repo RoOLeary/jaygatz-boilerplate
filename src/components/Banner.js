dd3c2fa6eb9f844ba2a039fc7367f3673bc142aa
Ronan <roleary81@gmail.com>
2019-02-10 01:52:00 +0100

    bit more gatsby plugin madness

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import Typing from 'react-typing-animation';
 
 const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, I'm Ro</h1>
+                <Typing>
+                <h1>Hi, I'm Ronan...</h1>
+                <Typing.Delay ms={2000} />
+                <Typing.Backspace count={20} />
+                <Typing.Delay ms={1000} />
+                <h1>I make websites</h1>
+                </Typing>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur <br />adipisicing elit. Suscipit, tenetur.</p>
                 <ul className="actions">
                     <li><AnchorLink className="button next scrolly" href='#one'>Get Started</AnchorLink></li>
                 </ul>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,6 +10,7 @@ const Menu = (props) => (
                 <li><Link onClick={props.onToggleMenu} to="/landing">Landing</Link></li>
                 <li><Link onClick={props.onToggleMenu} to="/generic">Generic</Link></li>
                 <li><Link onClick={props.onToggleMenu} to="/elements">Elements</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/blog">Musings</Link></li>
             </ul>
             <ul className="actions vertical">
                 <li><a href="#" className="button special fit">Get Started</a></li>
