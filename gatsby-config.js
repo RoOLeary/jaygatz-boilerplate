7d6a3d4c6e023d43e453db5b28f127b0ff2cfd7c
Ronan <roleary81@gmail.com>
2019-02-10 12:47:05 +0100

    removede deprecated plugin and go again

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: "Gatsby Starter - Forty V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Forty by HTML5 UP"
+    author: "Ro",
+    description: "Something to do on a Sunday"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
