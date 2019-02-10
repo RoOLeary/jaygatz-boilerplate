4cd32fa5811b86456ac6390de418b598402f11ea
Ronan <roleary81@gmail.com>
2019-02-10 12:39:54 +0100

    retry netlify commit

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ const NotFoundPage = () => (
     <div id="main" className="alt">
         <section id="one">
             <div className="inner">
-                <h1>NOT FOUND</h1>
+                <h1>OH NO! 4 OH 4! FILE NOT FOUND</h1>
                 <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
             </div>
         </section>
